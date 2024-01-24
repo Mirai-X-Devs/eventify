4bbbbfabdeda6824b74bee586af986b45f732345
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2024-01-24 20:21:47 -0300

    feat: add header with icon and login button

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,24 @@
+import Link from "next/link"
+import Image from "next/image"
+import { Button } from "../ui/button"
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <header className="w-full border-b">
+            <div className="wrapper flex items-center justify-between">
+                <Link href='/' className="w-36">
+                    <Image src='/assets/images/logo.svg' width={64} height={38} alt='Eventify Logo'/>
+                </Link>
+                <div className="flex w-32 justify-end gap-3">
+                    <Button className=" rounded-full" size='lg'>
+                        <Link href='/sign-in'>
+                            Login
+                        </Link>
+                        
+                    </Button>
+                </div>
+            </div>
+        </header>
     )
 }
 
